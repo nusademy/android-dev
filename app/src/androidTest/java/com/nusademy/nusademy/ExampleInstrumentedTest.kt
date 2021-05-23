a0abbaf3ee1b5f965e7082dca88738a2ba647471
Nearueki <nearueki@gmail.com>
2021-05-23 21:28:54 +0700

    Fix Instrument Test

--- app/src/androidTest/java/com/nusademy/nusademy/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/nusademy/nusademy/ExampleInstrumentedTest.kt
@@ -20,6 +20,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.nearueki.nusademy", appContext.packageName)
+        assertEquals("com.nusademy.nusademy", appContext.packageName)
     }
 }
